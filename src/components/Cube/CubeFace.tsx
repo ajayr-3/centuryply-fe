47c329c86591076c496a7897b6b2c8befde2d08f
ajay <ajay.ranga.mr.unique@gmail.com>
2024-05-28 13:46:07 +0530

    build error fixed

--- src/components/Cube/CubeFace.tsx
+++ src/components/Cube/CubeFace.tsx
@@ -1,15 +1,12 @@
 import { useGLTF } from '@react-three/drei';
 import { Euler, Vector3 } from '@react-three/fiber';
-import { useLayoutEffect, useState } from 'react';
+import { GLTFResult } from '../../types';
 
 const CubeFace = ({
   color,
   position,
   rotation,
   placeMent,
-  compartmentHeight,
-  compartmentWidth,
-  compartmentDepth,
 }: {
   color?: string;
   position?: Vector3;
@@ -19,14 +16,15 @@ const CubeFace = ({
   compartmentWidth: number;
   compartmentDepth: number;
 }) => {
-  const { nodes, materials } = useGLTF('CubeFace/CubeFace-transformed.glb');
+  const { nodes, materials } = useGLTF(
+    'CubeFace/CubeFace-transformed.glb'
+  ) as GLTFResult;
   return (
     <mesh
       geometry={nodes.Cube.geometry}
       material={materials.Material}
       position={position}
       rotation={rotation}
-      // scale={1}
       scale={(() => (placeMent === 'front' ? 0.8 : 1))()}
       material-color={color}>
       <meshStandardMaterial attach='material' color={color} />
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,15 @@
 declare module '*.gltf';
+
+declare module '*.glb' {
+  const content: string;
+  export default content;
+}
+
+export type GLTFResult = GLTF & {
+  nodes: {
+    CubeFaceMesh: Mesh;
+  };
+  materials: {
+    Material: THREE.Material;
+  };
+};
